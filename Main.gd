a5fe76b1d07d2c71fa5337525d6c9a38b98d4bc1
Jade1724 <haruka.i1724@gmail.com>
2022-03-22 11:37:33 +1300

    Attached a script to Main scene and instantiated randomized Mobs

--- Main.gd
+++ Main.gd
@@ -1,2 +1,21 @@
 extends Node
 
+export (PackedScene) var mob_scene
+
+func _ready():
+	randomize()
+
+func _on_MobTimer_timeout():
+	# Create a Mob instance and add it to the scenen.
+	var mob = mob_scene.instance()
+	
+	# Choose a random location on Path2D.
+	# We store the reference to the SpawnLocation node.
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	# And give it a random offset.
+	mob_spawn_location.unit_offset = randf()
+	
+	var player_position = $Player.transform.origin
+	
+	add_child(mob)
+	mob.initialize(mob_spawn_location.translation, player_position)
